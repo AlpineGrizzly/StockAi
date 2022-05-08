88635f59d4c566a02381da827c28edf71e96f3a7
Dalton Kinney <daltonckinney@gmail.com>
2022-05-08 14:37:23 -0400

    Add project files.

---
+++ StockGame.cpp
@@ -0,0 +1,15 @@
+// StockGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+/*Ideas to consider
+ * Want to develop ai's that will learn as you play
+ * Farther you go the faster and better you will have to be at choosing to invest
+ * Random world invents and news will affect how certain investors invest 
+ */
+
+int main()
+{
+    cout << "----------- Stock Game Test --------- \n By Dalton Kinney ";
+}
